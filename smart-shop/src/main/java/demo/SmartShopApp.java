13bd9218f0f6cc3b76e52f23e632749d589ab96b
momoNiti <60070041@kmitl.ac.th>
2019-09-08 14:35:20 +0700

    check

--- smart-shop/src/main/java/demo/SmartShopApp.java
+++ smart-shop/src/main/java/demo/SmartShopApp.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,49 +20,67 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @EnableAutoConfiguration
 public class SmartShopApp {
+	User u = User.getInstance();
+	@RequestMapping(value = "/set/{firstName}/{lastName}/{phone}", method = RequestMethod.GET)
+	public User setUser(@PathVariable String firstName, @PathVariable String lastName, @PathVariable String phone) {
+		u.setFirstName(firstName);
+		u.setLastName(lastName);
+		u.setPhone(phone);
+		return u;
+	}
 	@RequestMapping("/")
 	String home() {
-		return "Welcome to Bakery Smart Shop :) <br>" + "to see all product : /menu <br>"
+		if(u.getFirstName()==null) {
+			if(u.getLastName()==null) {
+				if(u.getPhone()==null) {
+					return "Please set your name and phone number <----" +
+				"/set/{firstName}/{lastName}/{phone}";
+				}
+			}
+		}
+		return "Welcome to Bakery Smart Shop :) <br>" 
+		+ u.getFirstName() + "  " + u.getLastName() + "  " + u.getPhone() +"<br>" 
+		+ "to see all product : /menu <br>"
 				+ "to see detail of product : /menu/{ID}   <-- ID of each product <br>"
 				+ "to add menu : /add{name}/{flavor}/{price} <-- name - name of product, flavor - flavor of product, price - price of product<br>"
 				+ "to delete menu : /delete/{ID} <-- ID of each product";
 	}
 
 	@RequestMapping("/menu")
-	List<Bakery> menu() {
-		return ListBakery.getAllBakery();
+	public ResponseEntity<List<Bakery>> menu(){
+//	List<Bakery> menu() {
+		return new ResponseEntity<List<Bakery>>(ListBakery.getAllBakery(), HttpStatus.OK);
 	}
-
 	@RequestMapping(value = "/menu/{ID}", method = RequestMethod.GET)
-	Bakery menu(@PathVariable int ID) {
-		return ListBakery.getBakery(ID);
+	public ResponseEntity<Bakery> menu(@PathVariable int ID) {
+		return new ResponseEntity<Bakery>(ListBakery.getBakery(ID), HttpStatus.OK);
 	}
 	
 	@RequestMapping(value = "/add/{name}/{flavor}/{price}", method = RequestMethod.GET)
-	 List<Bakery> add(@PathVariable String name, @PathVariable String flavor, @PathVariable float price) {
-		return ListBakery.addBakery(name, flavor, price);
+	public ResponseEntity<List<Bakery>> add(@PathVariable String name, @PathVariable String flavor, @PathVariable float price) {
+		return new ResponseEntity<List<Bakery>>(ListBakery.addBakery(name, flavor, price), HttpStatus.OK);
 	}
 	
-	@RequestMapping(value = "/delete/{ID}", method = RequestMethod.GET)
-	List<Bakery> delete(@PathVariable int ID){
-		return ListBakery.deleteIdBakery(ID);
+	@RequestMapping(value = "/delete/{ID}", method = RequestMethod.DELETE)
+	public ResponseEntity<List<Bakery>> delete(@PathVariable int ID){
+		return new ResponseEntity<List<Bakery>>(ListBakery.deleteIdBakery(ID), HttpStatus.OK);
 	}
 	
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmartShopApp.class, args);
-		User u = User.getInstance();
-//		initial
-		u.setFirstName("Niti");
-		u.setLastName("Jirakarnwuttikrai");
-		u.setPhone("0617689874");
-//		print check
-		System.out.println(u.getFirstName());
-		System.out.println(u.getLastName());
-		System.out.println(u.getPhone());
-//		check instance 
-		User a = User.getInstance();
-		System.out.println(a.getFirstName());
+//		User u = User.getInstance();
+////		initial
+//		u.setFirstName("Niti");
+//		u.setLastName("Jirakarnwuttikrai");
+//		u.setPhone("0617689874");
+////		print check
+//		System.out.println(u.getFirstName());
+//		System.out.println(u.getLastName());
+//		System.out.println(u.getPhone());
+////		check instance 
+//		User a = User.getInstance();
+//		System.out.println(a.getFirstName());
 		
 	}
 }
